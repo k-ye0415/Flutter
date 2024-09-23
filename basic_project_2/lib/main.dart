2e850d07f28a4ee83b4ef9b265009ccb267a971e
yejin.kang <yejin.kang@ipageon.com>
2024-09-23 13:52:53 +0900

    20240923
    /**
     * project 내부의 이미지 사용할 때 필요한 것.
     * 1. pubspec.yaml 에서
     *    assets:
        - assets/ic_check_off.svg
        - assets/test_image.jpg
        처럼 경로를 지정해줘야함.
     *
     * 2. 실제 사용할 dart 에서 위젯으로 불러온다.
     *
     *
     * SVG 파일을 사용할 때,
     * 1. pubspec.yaml 에서 dependencies 를 추가해야한다.
     *  터미널 명령어 : flutter pub add flutter_svg
     *  자동으로 추가 된다.
     *
     *  2. 실제 사용할 dart 에서 SvgPicture.asset 해준다.
     * */

--- basic_project_2/lib/main.dart
+++ basic_project_2/lib/main.dart
@@ -1,125 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,// 오른쪽 상단의 debug banner 없애는 것.
+      title: "20240923",
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Grade extends StatelessWidget {
+  const Grade({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("20240923"),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start, // 정렬
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/test_image.jpg"),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0, // divider 의 두께가 아니고, 해당 위젯을 기준으로 위/아래의 margin 값을 지정. 여기서는 위 30, 아래 30 으로 보면 됨.
+              color: Colors.grey[850],
+              thickness: 0.5, // 선의 두께
+              // endIndent:
+              //     30.0, // 끝에서 어느 정도 떨어져야 하는지 // 그냥...... padding 에다가 하면 안되낭...
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Name",
+              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
             ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "20240923",
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold // 글자 굵기
+                  ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "Name LEVEL",
+              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "14",
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold // 글자 굵기
+                  ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "using lightsaber",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "face hero tatto",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "fire flames",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            SvgPicture.asset("assets/ic_check_off.svg"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+/**
+ * project 내부의 이미지 사용할 때 필요한 것.
+ * 1. pubspec.yaml 에서
+ *    assets:
+    - assets/ic_check_off.svg
+    - assets/test_image.jpg
+    처럼 경로를 지정해줘야함.
+ *
+ * 2. 실제 사용할 dart 에서 위젯으로 불러온다.
+ *
+ *
+ * SVG 파일을 사용할 때,
+ * 1. pubspec.yaml 에서 dependencies 를 추가해야한다.
+ *  터미널 명령어 : flutter pub add flutter_svg
+ *  자동으로 추가 된다.
+ *
+ *  2. 실제 사용할 dart 에서 SvgPicture.asset 해준다.
+ * */
