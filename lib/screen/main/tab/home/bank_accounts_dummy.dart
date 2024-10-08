4d4bb8b0057189424c5cedeb10df47f4d46aedf3
yejin <ye_s_jin@naver.com>
2024-10-08 18:23:59 +0900

    [PastCampus] Part3 ch02.ttos - dummy data 추가

---
+++ lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -0,0 +1,16 @@
+import 'package:fast_app_base/screen/main/tab/home/banks_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank_accound.dart';
+
+final bankAccountShinhan = BankAccount(bankShinhan, 3000000);
+final bankAccountKakao1 = BankAccount(bankKakao, 30021240000);
+final bankAccountKakao2 = BankAccount(bankKakao, 32342675369, accountTypeName: "경조사 통장");
+final bankAccountKakao3 = BankAccount(bankKakao, 3002128237240000, accountTypeName: "데이트 통장");
+final bankAccountToss = BankAccount(bankTtoss, 3000025600);
+
+final bankAccounts = [
+  bankShinhan,
+  bankAccountKakao1,
+  bankAccountKakao2,
+  bankAccountKakao3,
+  bankAccountToss,
+];
