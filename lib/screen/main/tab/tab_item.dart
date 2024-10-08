2aa3e7ba3255d6a342355115f90b36e6d021b2d6
yejin <ye_s_jin@naver.com>
2024-10-08 18:03:32 +0900

    [PastCampus] Part3 ch02.ttos - bottomNavigate

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -1,12 +1,21 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/screen/main/tab/favorite/f_favorite.dart';
+import 'package:fast_app_base/screen/main/tab/all/f_all.dart';
+import 'package:fast_app_base/screen/main/tab/benefit/f_benefit.dart';
 import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
+import 'package:fast_app_base/screen/main/tab/stock/f_stock.dart';
+import 'package:fast_app_base/screen/main/tab/ttosspay/f_ttosspay.dart';
 import 'package:flutter/material.dart';
 
 enum TabItem {
   home(Icons.home, '홈', HomeFragment()),
-  favorite(Icons.star, '즐겨찾기', FavoriteFragment(isShowBackButton: false));
+  benefit(Icons.star, '혜택', BenefitFragment()),
+  ttosspay(Icons.payment, '토스페이', TtosspayFragment()),
+  stock(Icons.candlestick_chart, '주식', StockFragment()),
+  all(Icons.menu, '전체', AllFragment());
 
+  /// custom icon 설정 시
+  /// String imagePath;
+  ///         해당 value 는 assets/...// 경로 지정.
   final IconData activeIcon;
   final IconData inActiveIcon;
   final String tabName;
