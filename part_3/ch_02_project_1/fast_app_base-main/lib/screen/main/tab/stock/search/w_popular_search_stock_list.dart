bffb7db2c28ae6d40f0c86c0a7ee9241d095e85c
yejin <ye_s_jin@naver.com>
2024-10-11 17:31:19 +0900

    [PastCampus] Part3 ch03.ttoss Animation - Flutter 공식 animation 활용

--- part_3/ch_02_project_1/fast_app_base-main/lib/screen/main/tab/stock/search/w_popular_search_stock_list.dart
+++ part_3/ch_02_project_1/fast_app_base-main/lib/screen/main/tab/stock/search/w_popular_search_stock_list.dart
@@ -1,7 +1,9 @@
+import 'package:animations/animations.dart';
 import 'package:fast_app_base/app.dart';
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/dart/extension/datetime_extension.dart';
 import 'package:fast_app_base/screen/main/tab/stock/search/popular_stock_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/stock/search/s_stock_detail.dart';
 import 'package:fast_app_base/screen/main/tab/stock/search/w_popular_stock_item.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +23,18 @@ class PopularSearchStockList extends StatelessWidget {
         ),
         height20,
         ...popularStockList
-            .mapIndexed((element, index) => PopularStockItem(stock: element, index: index + 1))
+            .mapIndexed(
+              (element, index) => OpenContainer<bool>(
+                openColor: context.backgroundColor,
+                closedColor: context.backgroundColor,
+                openBuilder: (BuildContext context, VoidCallback _) {
+                  return StockDetailScreen(stockName: element.name);
+                },
+                closedBuilder: (BuildContext context, VoidCallback action) {
+                  return PopularStockItem(stock: element, index: index + 1);
+                },
+              ),
+            )
             .toList()
       ],
     ).pSymmetric(h: 20);
